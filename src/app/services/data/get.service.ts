2a6a34c7679673f4cbba0f5c0c9db3e395e43c9b
phamminhphu <phamminhphueur@gmail.com>
2020-04-22 16:04:11 +0700

    Catch error

--- src/app/services/data/get.service.ts
+++ src/app/services/data/get.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable , of } from 'rxjs';
+import { Observable , of, throwError } from 'rxjs';
 import { quiz } from '../../model/quiz'
 import { HttpClient } from '@angular/common/http';
 
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +14,22 @@ import { BehaviorSubject } from 'rxjs';
 export class GetService { 
   private messageSource = new BehaviorSubject('');
   currentMessage = this.messageSource.asObservable();
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient,  private toastr: ToastrService, public router : Router) { }
   getquiz(id) : Observable<quiz[]> {
-      return this.http.get<quiz[]>(`../../../assets/data/${id}.json`);
-  }
+      return this.http.get<quiz[]>(`../../../assets/data/${id}.json`)
+                      .pipe(
+                        catchError((err)=>{
+                          console.log(err);
+                          this.toastr.error('Data Not Available! Will fix soonest', 'We are sorry!',{
+                            positionClass:'toast-top-center', 
+                            timeOut: 2000,
+                          });
+                          this.router.navigate(['exam']);
+                          return throwError(err)
+                        })
+                      )                  
+    }
+
   changeMessage(message: string) {
     this.messageSource.next(message)
   }
